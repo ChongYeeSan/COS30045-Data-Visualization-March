ec5083a70c2eb27f38452610fe858f37d90896a9
kronchy coder <J25045644@student.newinti.edu.my>
2026-05-13 00:29:11 +0800

    Duplicated the images from previous Exercise and remove an error from the index file and rename the js file from main to script.js

--- Exercise 4/Exercise 4.4/js/scripts.js
+++ Exercise 4/Exercise 4.4/js/scripts.js
@@ -1,3 +1,18 @@
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(page => {
+        page.style.display = 'none';
+    });
+
+    document.getElementById(pageId).style.display = 'block';
+
+    document.querySelectorAll('.nav-links li').forEach(link => {
+        link.classList.remove('active');
+    });
+    document.getElementById("link-" + pageId).classList.add('active');
+}
+
+showPage('home');
+
 d3.csv("data/Energy_consumption_for_televion_in_Australia.csv", function(d) {
     return {
         brand: d.Brand_Reg,
