ce595ae598c50deeecbcb3ef394cf71c39dc5267
kronchy coder <J25045644@student.newinti.edu.my>
2026-05-21 22:02:22 +0800

    Removed some of the comments from Exercise 4.5 and removed duplicated codes.
    Changed the variable names for chart 1 and 2 for exercise 4.5 and fixed the margins of the chart due to alignment issue.

--- Exercise 4/Exercise 4.5/js/scripts.js
+++ Exercise 4/Exercise 4.5/js/scripts.js
@@ -26,45 +26,77 @@ d3.csv("data/Energy_consumption_for_televion_in_Australia.csv", function(d) {
     console.log("Min energy consumption:", d3.min(data, d => d.energyConsumption));
 });
 
-const width = 1200;
-const height = 600;
+const width1 = 1200;
+const height1 = 600;
+
+const margin1 = {top: 20, right: 20, bottom: 50, left: 60};
+
+const innerWidth1 = width1 - margin1.left - margin1.right;
+const innerHeight1 = height1 - margin1.top - margin1.bottom;
 
 const svg = d3.select("#chart1")
     .append("svg")
-    .attr("viewBox", `0 0 ${width} ${height}`);
+    .attr("viewBox", `0 0 ${width1} ${height1}`);
+
+const g1 = svg.append("g")
+    .attr("transform", `translate(${margin1.left}, ${margin1.top})`);
+
 
 const myData = [
-    {Screen_Tech: "LCD(LED)", avgPower: 73.40},
+    {Screen_Tech: "LCD", avgPower: 96.45},
     {Screen_Tech: "OLED", avgPower: 86.94},
-    {Screen_Tech: "LCD", avgPower: 96.45}
+    {Screen_Tech: "LCD(LED)", avgPower: 73.40}
+    
+    
 ];
 
-const barHeight = 60;
-const spacing = 20;
+const xBar = d3.scaleBand() // X axis values
+    .domain(myData.map(d => d.Screen_Tech))
+    .range([0, innerWidth1])
+    .padding(0.2);
 
-svg.selectAll("rect")
+const yBar = d3.scaleLinear() // Y axis values
+    .domain([0, d3.max(myData, d => d.avgPower)])
+    .nice()
+    .range([innerHeight1, 0]);
+
+// Displays the bar 
+g1.selectAll("rect")
     .data(myData)
     .join("rect")
-    .attr("x", 0)
-    .attr("y", (d, i) => i * (barHeight + spacing))
-    .attr("width", d => d.avgPower * 10)
-    .attr("height", barHeight)
+    .attr("x", d => xBar(d.Screen_Tech))
+    .attr("y", d => yBar(d.avgPower))
+    .attr("width", xBar.bandwidth())
+    .attr("height", d => innerHeight1 - yBar(d.avgPower))
     .attr("fill", "steelblue");
 
-svg.selectAll("text")
+// Adds labels beside the chart    
+g1.selectAll("text") 
     .data(myData)
     .join("text")
-    .attr("x", d => d.avgPower * 10 + 10)
-    .attr("y", (d, i) => i * (barHeight + spacing) + barHeight / 2 + 5)
-    .text(d => `${d.Screen_Tech}: ${d.avgPower} kWh/year`)  
+    .attr("x", d => xBar(d.Screen_Tech) + xBar.bandwidth() / 2)
+    .attr("y", d => yBar(d.avgPower) - 5)
+    .attr("text-anchor", "middle")
+    .text(d => `${d.Screen_Tech}: ${d.avgPower}`)
     .attr("fill", "black")
-    .attr("font-size", "16")
+    .attr("font-size", 16);
 
 // Chart 2 - Brands with the most models
 
+const margin2 = {top: 20, right: 20, bottom: 50, left: 60};
+
+const width2 = 1200;
+const height2 = 600;
+
+const innerWidth2 = width2 - margin2.left - margin2.right;
+const innerHeight2 = height2 - margin2.top - margin2.bottom;
+
 const svg2 = d3.select("#chart2")
     .append("svg")
-    .attr("viewBox", `0 0 ${width} ${height}`);
+    .attr("viewBox", `0 0 ${width2} ${height2}`);
+
+const g2 = svg2.append("g")
+    .attr("transform", `translate(${margin2.left}, ${margin2.top})`);
 
 const myData2 = [
     {brand: "Samsung Electronics", modelCount: 731},
@@ -73,20 +105,33 @@ const myData2 = [
     {brand: "Hisense", modelCount: 323},
 ];
 
-svg2.selectAll("rect")
+const xBar2 = d3.scaleBand() // X axis values
+    .domain(myData2.map(d => d.brand))
+    .range([0, innerWidth2])
+    .padding(0.2);
+
+const yBar2 = d3.scaleLinear() // Y axis values
+    .domain([0, d3.max(myData2, d => d.modelCount)])
+    .nice()
+    .range([innerHeight2, 0]);
+
+// Displays the bar 
+g2.selectAll(".bar")
     .data(myData2)
     .join("rect")
-    .attr("x", (d, i) => i * (barHeight + spacing))
-    .attr("y", d => height - d.modelCount)
-    .attr("width", barHeight)
-    .attr("height", d => d.modelCount * 1.5)
+    .attr("x", d => xBar2(d.brand))
+    .attr("y", d => yBar2(d.modelCount))
+    .attr("width", xBar2.bandwidth())
+    .attr("height", d => innerHeight2 - yBar2(d.modelCount))
     .attr("fill", "steelblue");
 
-svg2.selectAll("text") 
+// Adds labels beside the chart    
+g2.selectAll("text") 
     .data(myData2)
     .join("text")
-    .attr("x", d, i => d.modelCount * 1.5 + 10)
-    .attr("y",(d,i)=> i *(barHeight + spacing) + barHeight / 2 + 5)
+    .attr("x", d => xBar2(d.brand) + xBar2.bandwidth() / 2)
+    .attr("y", d => yBar2(d.modelCount) - 5)
+    .attr("text-anchor", "middle")
     .text(d => `${d.brand}: ${d.modelCount}`)
     .attr("fill", "black")
     .attr("font-size", 16);
